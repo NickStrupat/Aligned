a42d8a3045edd2a83e1bd10d37d923ed2939ad1c
NickStrupat <nick.strupat@gmail.com>
2014-04-29 23:35:10 -0400

    Moved to a single type, Aligned, with template specialization. The templates specialize for T and T[], each with specializations for alignment known at compile time or at run-time. Also a specialization for when T[Size] is known at compile time.

--- Aligned.hpp
+++ Aligned.hpp
@@ -2,12 +2,18 @@
 #define ALIGNED_HPP_INCLUDED
 
 #include <cstdint>
+#include <memory>
 #include "RoundUp.hpp"
 
-template <typename T, size_t alignment = 64>
-struct Aligned {
-private:
-	enum { SizeOfTPaddedToAlignment = RoundUp<sizeof(T), alignment>::value };
+//#define ALIGNED_BYTES_SIZE(sizeOfTPaddedToAlignment) sizeof(T) + sizeOfTPaddedToAlignment - 1
+//#define ALIGNED_ALIGNMENT_CAST(expressionToCast) *reinterpret_cast<T *>(expressionToCast)
+//#define ALIGNED_ALIGNMENT(pBytes, sizeOfTPaddedToAlignment) ALIGNED_ALIGNMENT_CAST(intptr_t(pBytes) + (sizeOfTPaddedToAlignment - 1) & ~intptr_t(sizeOfTPaddedToAlignment - 1))
+//#define ALIGNED_INDEX_ALIGNMENT(pBytes, sizeOfTPaddedToAlignment, index) (ALIGNED_ALIGNMENT(pBytes, sizeOfTPaddedToAlignment) + (SizeOfTPaddedToAlignment * index))
+
+template<typename T, size_t Alignment = -1>
+class Aligned {
+	static const size_t SizeOfTPaddedToAlignment = RoundUp<sizeof(T), Alignment>::value;
+	//uint8_t bytes[sizeof(T) +SizeOfTPaddedToAlignment - 1];
 	uint8_t bytes[sizeof(T) + SizeOfTPaddedToAlignment - 1];
 public:
 	Aligned() {}
@@ -15,11 +21,65 @@ public:
 		reference() = value;
 	}
 	T & reference() {
-		return *reinterpret_cast<T *>((intptr_t) bytes + (SizeOfTPaddedToAlignment - 1) & ~intptr_t(SizeOfTPaddedToAlignment - 1));
+		return *reinterpret_cast<T *>(intptr_t(bytes) + (SizeOfTPaddedToAlignment - 1) & ~intptr_t(SizeOfTPaddedToAlignment - 1));
+		//return ALIGNED_ALIGNMENT(bytes, SizeOfTPaddedToAlignment);
 	}
 	T const & reference() const {
 		return reference();
 	}
 };
+
+template<typename T>
+class Aligned<T, -1> {
+	size_t const sizeOfTPaddedToAlignment;
+	std::unique_ptr<uint8_t[]> const pBytes;
+public:
+	Aligned(size_t alignment) : sizeOfTPaddedToAlignment(roundUp(sizeof(T), alignment)), pBytes(new uint8_t[sizeof(T) + sizeOfTPaddedToAlignment - 1]) {}
+	Aligned(T const & value, size_t alignment) : Aligned(alignment) { reference() = value; }
+	T & reference() {
+		return *reinterpret_cast<T *>(intptr_t(pBytes.get()) + (sizeOfTPaddedToAlignment - 1) & ~intptr_t(sizeOfTPaddedToAlignment - 1));
+		//return ALIGNED_ALIGNMENT(pBytes.get(), sizeOfTPaddedToAlignment);
+	}
+};
+
+template<typename T, size_t Size, size_t Alignment>
+class Aligned<T[Size], Alignment> {
+	static const size_t SizeOfTPaddedToAlignment = RoundUp<sizeof(T), Alignment>::value;
+	uint8_t bytes[sizeof(T) + (SizeOfTPaddedToAlignment * Size) - 1];
+public:
+	Aligned() {};
+	T & operator[](size_t index) {
+		return *reinterpret_cast<T *>((intptr_t(bytes) + (SizeOfTPaddedToAlignment - 1) & ~intptr_t(SizeOfTPaddedToAlignment - 1)) + (SizeOfTPaddedToAlignment * index));
+		//return ALIGNED_INDEX_ALIGNMENT(bytes, SizeOfTPaddedToAlignment, index);
+	}
+	T const & operator[](size_t index) const { return operator[](index); }
+};
+
+template<typename T, size_t Alignment>
+class Aligned<T[], Alignment> {
+	static const size_t SizeOfTPaddedToAlignment = RoundUp<sizeof(T), Alignment>::value;
+	std::unique_ptr<uint8_t[]> const pBytes;
+	Aligned();
+public:
+	Aligned(size_t size) : pBytes(new uint8_t[sizeof(T) + (SizeOfTPaddedToAlignment * size) - 1]) {}
+	T & operator[](size_t index) {
+		return *reinterpret_cast<T *>((intptr_t(pBytes.get()) + (SizeOfTPaddedToAlignment - 1) & ~intptr_t(SizeOfTPaddedToAlignment - 1)) + (SizeOfTPaddedToAlignment * index));
+		//return ALIGNED_INDEX_ALIGNMENT(pBytes.get(), SizeOfTPaddedToAlignment, index);
+	}
+};
+
+template<typename T>
+class Aligned<T[], -1> {
+	size_t const sizeOfTPaddedToAlignment;
+	std::unique_ptr<uint8_t> const pBytes;
+	Aligned();
+public:
+	Aligned(size_t size, size_t alignment) : sizeOfTPaddedToAlignment(roundUp(sizeof(T), alignment)), pBytes(new uint8_t[sizeof(T) + (sizeOfTPaddedToAlignment * size) - 1]) {}
+	T & operator[](size_t index) {
+		return *reinterpret_cast<T *>((intptr_t(pBytes.get()) + (sizeOfTPaddedToAlignment - 1) & ~intptr_t(sizeOfTPaddedToAlignment - 1)) + (sizeOfTPaddedToAlignment * index));
+	}
+};
+
+#undef ALIGNMENT_EQUATION
 
 #endif
--- RoundUp.hpp
+++ RoundUp.hpp
@@ -1,6 +1,11 @@
 #ifndef ROUNDUP_HPP_INCLUDED
 #define ROUNDUP_HPP_INCLUDED
 
+template<typename T>
+inline T roundUp(T from, T to) {
+	return from + to - 1 - (from - 1) % to;
+}
+
 template<size_t From, size_t To>
 struct RoundUp {
 	enum { value = From + To - 1 - (From -1) % To };
