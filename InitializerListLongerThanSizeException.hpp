e7f77fc6ff0c55c141520f1b662da4fb7e04dada
Nick Strupat <nick.strupat@gmail.com>
2016-04-22 23:17:27 -0400

    add exception class

---
+++ InitializerListLongerThanSizeException.hpp
@@ -0,0 +1,8 @@
+#ifndef INITIALIZERLISTLONGERTHANSIZEEXCEPTION_HPP_INCLUDED
+#define INITIALIZERLISTLONGERTHANSIZEEXCEPTION_HPP_INCLUDED
+
+#include <exception>
+
+class InitializerListLongerThanSizeException : std::exception {};
+
+#endif
